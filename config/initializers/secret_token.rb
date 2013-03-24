6fd2a6f2ec6bfa0bd675c8fb77e6237a77dc704f
Alexandre Beaulieu <alxbl03@gmail.com>
2013-03-24 01:31:50 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SegfaultMe::Application.config.secret_token = 'f3663592819aa83f4f92e3e337939784417545abf6722ecb6cc7cead37e297047c6b50cf6453a7c99887c5f1544b7d076914969693328601c0473ee517f73a67'
