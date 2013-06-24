41eaff6bd07984e6a7970f60a7c2baec2f38eeda
Alexandre Beaulieu <alxbl03@gmail.com>
2013-06-24 23:14:14 +0000

    Autodetect locale and cache it on first visit.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,24 @@ class ApplicationController < ActionController::Base
   before_filter :set_locale
 
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+    # On the first visit, look at Accept-language and select the preferred language
+    if !cookies[:lang_autodetected]
+      I18n.locale = extract_locale_from_header
+      if I18n.locale != I18n.default_locale && Language.find_by_code(I18n.locale)
+        cookies[:lang_autodetected] = true
+        redirect_to "/#{I18n.locale}#{request.fullpath}" # Redirect to the same page, different locale.
+      end
+    else
+      I18n.locale = params[:locale] || I18n.default_locale
+    end
   end
 
   def default_url_options(options={})
-    { :locale => I18n.locale == :en ? nil : I18n.locale } # Don't put a locale for English
+    { :locale => I18n.locale == I18n.default_locale ? nil : I18n.locale } # Don't put a locale for English
+  end
+
+  private
+  def extract_locale_from_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
   end
 end
