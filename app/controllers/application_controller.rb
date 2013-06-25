dc88134b04d4e5f2a2bf51a76d531e69e96e5d16
Alexandre Beaulieu <alxbl03@gmail.com>
2013-06-25 00:35:24 +0000

    Arrrr... locales.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,15 +5,17 @@ class ApplicationController < ActionController::Base
 
   def set_locale
     # On the first visit, look at Accept-language and select the preferred language
-    if !session[:lang_autodetected]
+    if !session[:locale]
       I18n.locale = extract_locale_from_header
       if I18n.locale != I18n.default_locale && Language.find_by_code(I18n.locale)
-        session[:lang_autodetected] = true
+        session[:locale] = I18n.locale
+        logger.debug "Locale set to #{I18n.locale}"
         redirect_to url_for locale: I18n.locale # Redirect to the same page, different locale.
       end
-    else
-      I18n.locale = params[:locale] || I18n.default_locale
     end
+    # Update the locale if it changed between requests.
+    I18n.locale = session[:locale] = params[:locale] || I18n.default_locale if session[:locale] != params[:locale]
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 
   def default_url_options(options={})
