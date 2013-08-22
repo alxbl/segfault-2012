5fcf6a32e73536fe966f00d4c76e00f36a0573c6
Alexandre Beaulieu <alxbl03@gmail.com>
2013-08-22 08:07:54 +0000

    Removed unused frequency cache to avoid useless SQL queries.

--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -1,4 +1,4 @@
 class Tagging < ActiveRecord::Base
   belongs_to :translation
-  belongs_to :tag, :counter_cache => :freq
+  belongs_to :tag #, :counter_cache => :freq # Disable counter cache for now
 end
